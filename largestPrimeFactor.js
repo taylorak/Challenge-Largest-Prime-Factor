eb2bf12902022a0b7f09ae941ccab22001c4e41e
Taylor Kennedy <taykenned@gmail.com>
2016-03-09 10:40:47 -1000

    Adds optimized largestPrimeFactor

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,21 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
-  // do your work here
+
+  while(n % 2 === 0) {
+      primeNumber = 2;
+      n /= 2;
+  }
+
+  for( var i = 3; i < Math.sqrt(n); i = i + 2) {
+      while(n % i === 0) {
+          primeNumber = i;
+          n /= i;
+      }
+  }
+
+  if(n > 2) {
+      primeNumber = n;
+  }
 
   return primeNumber;
 };
